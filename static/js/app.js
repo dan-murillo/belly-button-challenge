9f8ac3cae9395ca6b155eb3f0bec673d93a7e03e
Daniel Murillo <danielmgiant@gmail.com>
2023-04-20 23:07:54 -0600

    Uploaded final code

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,7 @@
+// Made a const declaration with the URL of the JSON. 
 const myURL = 'https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json'
 
+// Read the JSON with D3 and once read, created a function to get the participant ID and to change the data of the dashboard.
 d3.json(myURL).then(({names}) => {
 
     names.forEach(id => {
@@ -11,20 +13,26 @@ d3.json(myURL).then(({names}) => {
 
 // Used the function expression 'optionChanged' to limit where the function is available and keep my global scope light whenever the ID is changed.
 const optionChanged = () => {
+    // Took the first ID.
     let selectedID = d3.select('select').node().value;
 
     d3.json(myURL).then(({metadata, samples}) => {
+        // Declared a variable to hold the 'metadata' object.
         let participantData = metadata.filter(obj => obj.id == selectedID)[0];
 
+        // Declared a variable to hold the 'samples' object.
         let sample = samples.filter(obj => obj.id == selectedID)[0];
 
+        // Selected the HTML 'panel-body' class and appended the participant's metadata.
         d3.select('.panel-body').html('');
         Object.entries(participantData).forEach(([key, val]) => {
             d3.select('.panel-body').append('h6').text(`${key}: ${val}`)
         });
 
+        // Declared a variable for each array to be used in the charts.
         let {otu_ids, otu_labels, sample_values} = sample;
 
+        // Declared a global variable to hold the bar chart data.
         var data = [{
             x: sample_values.slice(0,10).reverse(),
             y: otu_ids.slice(0,10).reverse().map(x => `OTU ${x}`),
@@ -33,8 +41,10 @@ const optionChanged = () => {
             type: 'bar'
         }];
 
+        // Plotted the bar chart.
         Plotly.newPlot('bar', data);
 
+        // Declared a global variable to hold the bubble chart data.
         var bubbles = [{
             x: otu_ids,
             y: sample_values,
@@ -47,12 +57,14 @@ const optionChanged = () => {
             text: otu_labels
         }];
 
+        // Declared a global variable to hold the x axis label of the bubble chart.
         var layout = {
             xaxis: {
                 title: 'OTU ID'
             }
         };
         
+        // Plotted the bubble chart.
         Plotly.newPlot('bubble', bubbles, layout);
     })
 }
